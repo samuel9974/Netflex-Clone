8ed98d0abf1b28388e2f17f03c0c39a6e852f61e
samuwa95 <samisancho1@gmail.com>
2026-01-27 14:44:44 +0200

    Add Banner component and update API key usage
    Introduced a new Banner component with associated styles and integrated it into the Home page. Updated environment variable and API key usage to use Vite conventions. Adjusted requests.js to reference the new VITE_API_KEY.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../compenents/Header/Header.jsx'
 import Footer from '../../compenents/Footer/Footer.jsx'
+import Banner from '../../compenents/Banner/Banner.jsx'
 
 const Home = () => {
   return (
     <div> 
        <Header />
+       <Banner />
        <Footer />
     </div>
   )
